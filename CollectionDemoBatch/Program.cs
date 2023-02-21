ab7640961976fc10cade73c5a09a0e46121afb40
Snehal979 <bansodsnehal535@gmail.com>
2023-02-21 23:17:26 +0530

    [Snehal]Add: List collection

--- CollectionDemoBatch/Program.cs
+++ CollectionDemoBatch/Program.cs
@@ -7,7 +7,37 @@ namespace CollectionDemoBatch
         public static void Main(string[] args)
         {
             Console.WriteLine("Collection Demo");
-            Console.ReadLine();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("hint 1.list 2.exit");
+                int num = Convert.ToInt16(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        DoListDemo();
+                        break;
+                    case 2:
+                        Console.WriteLine("Exit");
+                        flag=false;
+                        break;
+                }
+            }           Console.ReadLine();
+        }
+        private static void DoListDemo()
+        {
+            Console.WriteLine("List Collection");
+            List<string> list = new List<string>();
+            list.Add("Snehal");
+            list.Add("Mayur");
+            list.Add("Vaibhav");
+            list.Add("Raju");
+            list.Add("Lata");
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
