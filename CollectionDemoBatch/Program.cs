86f05ccf5bb19289ea3824f217d769a5c9fa0033
Snehal979 <bansodsnehal535@gmail.com>
2023-02-21 23:24:13 +0530

    [Snehal]Add: Stack collection

--- CollectionDemoBatch/Program.cs
+++ CollectionDemoBatch/Program.cs
@@ -10,7 +10,7 @@ namespace CollectionDemoBatch
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("hint 1.list 2.exit");
+                Console.WriteLine("hint 1.list 2.Stack 3.exit");
                 int num = Convert.ToInt16(Console.ReadLine());
                 switch (num)
                 {
@@ -18,6 +18,9 @@ namespace CollectionDemoBatch
                         DoListDemo();
                         break;
                     case 2:
+                        DoStackDemo();
+                        break;
+                    case 3:
                         Console.WriteLine("Exit");
                         flag=false;
                         break;
@@ -37,6 +40,23 @@ namespace CollectionDemoBatch
             foreach (string item in list)
             {
                 Console.WriteLine(item);
+            }
+        }
+        private static void DoStackDemo()
+        {
+            Console.WriteLine("Stack Collection"); //Last in First out
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Snehal");  //1
+            stack.Push("Mayur");  //2
+            stack.Push("Vaibhav");  //3
+            stack.Push("Raju");  //4
+            stack.Push("Lata"); //5
+            stack.Push("Hina"); //6
+            Console.WriteLine("Top of the element"+stack.Peek());//Top element
+            Console.WriteLine("the element pop is : "+stack.Pop()); //delect top element
+            foreach (var item in stack)   //display
+            {
+                Console.WriteLine(item); ;
             }
         }
     }
