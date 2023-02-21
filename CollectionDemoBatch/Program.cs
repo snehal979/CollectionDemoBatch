83730e52121811a90c60e747df2f47c5e84fd18e
Snehal979 <bansodsnehal535@gmail.com>
2023-02-21 23:02:19 +0530

    [Snehal]Add: Collection Demo

---
+++ CollectionDemoBatch/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace CollectionDemoBatch
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Collection Demo");
+            Console.ReadLine();
+        }
+    }
+}
